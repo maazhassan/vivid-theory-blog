e415eab15cf179793ca5604ad86fca9e5607d460
Maaz Hassan <h.maaz809@hotmail.com>
2024-03-02 08:28:49 -0700

    feat: add search functionality for blogs
    Suggestions are retrieved from the server at this point, but are not yet rendered. After 300ms of not typing, 4 suggestions are retrieved. Currently just printing them to the console.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { getBlogs } from "@/services/api";
 import { Blog } from "@/types/blog";
 import { useState, useEffect } from "react";
 import Pagination from "@/components/Pagination";
+import SearchBar from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,20 +13,30 @@ export default function BlogSearchPage() {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    searchBlogs("", currentPage, true);
+    searchBlogs("", currentPage);
   }, [currentPage]); // Fetch blogs when the currentPage changes
 
-  const searchBlogs = async (search: string, page: number, content: boolean) => {
+  const searchBlogs = async (search: string, page: number) => {
     try {
-      const { blogs: fetchedBlogs, total } = await getBlogs(search, page, content);
+      const { blogs: fetchedBlogs, total } = await getBlogs(search, page, 6, true);
       setBlogs(fetchedBlogs);
       setTotalPages(Math.ceil(total / 6));
     } catch (error) {
       console.error("Error fetching blogs:", error);
     }
   };
+
+  const handleInputChange = (search: string) => {
+    setSearch(search);
+  }
+
+  const handleSearch = () => {
+    setCurrentPage(1);
+    searchBlogs(search, 1);
+  }
 
   return (
     <main className={inter.className}>
@@ -34,7 +45,11 @@ export default function BlogSearchPage() {
       </Head>
       <div className="container mx-auto">
         <h1 className="text-4xl font-semibold my-4">Blog Search</h1>
-        
+        <SearchBar
+          search={search}
+          onInputChange={handleInputChange}
+          onSearch={handleSearch}
+        />
         <div className="flex flex-col justify-center">
           {blogs.map((blog) => (
             <BlogListItem key={blog.slug} blog={blog} className="my-2"/>
