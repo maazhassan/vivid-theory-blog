8cc9bdd4ae7929f2261d6f2533ee34df54463326
Maaz Hassan <h.maaz809@hotmail.com>
2024-03-03 12:45:00 -0700

    fix: fix bug with slug generation

--- backend/src/utils/slug.ts
+++ backend/src/utils/slug.ts
@@ -3,10 +3,10 @@ import { blog } from '../models/blog.model.js';
 export const getSlugFromTitle = async (title: string) => {
   const amount = await blog.count({ where: { title } });
 
-  title.toLowerCase().split(' ').join('-');
+  const slug = title.toLowerCase().split(' ').join('-');
 
   if (amount) {
-    return `${title}-${amount}`;
+    return `${slug}-${amount}`;
   }
-  return title;
+  return slug;
 }
