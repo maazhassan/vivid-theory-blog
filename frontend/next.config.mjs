3cca10c492b2f9e375c35b6c201b5fab6ec29bd5
Maaz Hassan <h.maaz809@hotmail.com>
2024-03-03 12:12:48 -0700

    build: add http remote pattern for images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,9 +6,12 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "**"
+      },
+      {
+        protocol: "http",
+        hostname: "**"
       }
     ],
-    domains: ["localhost"]
   }
 };
 
