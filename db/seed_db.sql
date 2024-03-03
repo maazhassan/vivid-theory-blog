2aef411b387035056350567d3e662ed30cc394a1
Maaz Hassan <h.maaz809@hotmail.com>
2024-03-03 15:13:29 -0700

    build: update test data

--- db/seed_db.sql
+++ db/seed_db.sql
@@ -1,21 +1,30 @@
 \c vivid_blog_challenge;
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Sample Blog Post 1', 'sample-blog-post-1', '<p>This is a sample blog post content in HTML format.</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Sample Blog Post 1', 'sample-blog-post-1', '<p>This is a sample blog post content in HTML format.</p>', 'mountains.jpeg', '2024-02-25', '2024-02-25', '2024-02-25');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Sample Blog Post 2', 'sample-blog-post-2', '<p>This is a sample blog post content in HTML format, again.</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Sample Blog Post 2', 'sample-blog-post-2', '<p>This is a sample blog post content in HTML format, again.</p>', 'mountains.jpeg', '2024-02-26', '2024-02-26', '2024-02-26');
+
+INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
+VALUES ('Sample Blog Post 3', 'sample-blog-post-3', '<p>This is a sample blog post content in HTML format, again, again.</p>', 'mountains.jpeg', '2024-02-27', '2024-02-27', '2024-02-27');
+
+INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
+VALUES ('Sample Blog Post 4', 'sample-blog-post-4', '<p>This is a sample blog post content in HTML format, again, again, again.</p>', 'mountains.jpeg', '2024-02-28', '2024-02-28', '2024-02-28');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
 VALUES ('Dragons and Dungeons', 'dragons-and-dungeons', '<p>The new and improved D&D.</p>', 'dragons-and-dungeons.jpg', '2024-02-29', '2024-02-29', '2024-02-29');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Hades Speedrunning', 'hades-speedrunning', '<p>This post will talk about how to go fast in a game called Hades.</p>', 'hades-speedrunning.png', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Hades Speedrunning', 'hades-speedrunning', '<div><p>This post will talk about how to go fast in a game called Hades. What is Hades? Hades is a rogue-like dungeon crawler developed by Supergiant games. If you want to go fast in Hades, simply follow these easy steps:</p> <ul> <li>Start the game</li> <li>Get Eris Rail</li> <li>Get Zeus</li> <li>Get Poseidon</li> <li>Get Sea Storm</li> <li>???</li> <li>Profit</li> </ul></div>', 'hades-speedrunning.png', '2024-03-01', '2024-03-01', '2024-03-01');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Burden of Memes', 'burden-of-memes', '<p>Will Bosi gave me an exclusive interview (in my dreams). Here''s what I learned...</p>', 'burden-of-memes.jpg', '2024-03-01', '2024-03-01', '2024-03-01');
+VALUES ('Burden of Memes', 'burden-of-memes', '<div><p>Will Bosi gave me an exclusive interview (in my dreams). Here''s what I learned:</p> <ul> <li>Be strong</li> <li>Don''t be not strong</li> <li>Be stronger than that</li> <li>Never do comps</li> <li>Learn one arm pull-ups and do them while you sleep</li> <li>Go to Norway and climb outdoors regardless of the weather</li> <li>Go back in time and start climbing as soon as you can walk</li> <li>Build a 1-1 replica of the boulder you''re working on so you can train all the time</li> <li>Train with Adam Ondra, Pete Whittaker, and Stefano Ghisolfi</li> </ul> <p> Following these simple steps, anyone can climb V17!</div>', 'burden-of-memes.jpg', '2024-03-02', '2024-03-02', '2024-03-02');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Random Thoughts', 'random-thoughts', '<p>Your stomach thinks all potatoes are mashed...</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Random Thoughts', 'random-thoughts', '<p>Your stomach thinks all potatoes are mashed...</p>', 'mountains.jpeg', '2024-02-28', '2024-02-28', '2024-02-28');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
 VALUES ('More Random Thoughts', 'more-random-thoughts', '<p>Someone studying atoms is just a bunch of atoms trying to understand themselves...</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+
+INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
+VALUES ('Lorem Ipsum', 'lorem-ipsum', '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id semper enim, in condimentum magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur aliquet quam ut urna porta dapibus. Cras vel mauris pharetra, rhoncus tellus et, sagittis neque. Aliquam ornare neque sit amet tellus aliquam, nec viverra ex maximus. Pellentesque quis congue justo. Morbi malesuada feugiat felis non mattis. Aenean ut velit non nulla auctor fermentum. Cras sollicitudin mollis leo eu aliquet. Curabitur vulputate metus sed tellus porttitor tristique. Duis egestas orci quis leo porttitor faucibus. Ut sit amet rutrum mi, venenatis feugiat libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur mollis vehicula dignissim. Sed eu tincidunt lorem.</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
