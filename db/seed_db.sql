730e868b788e2afa10b481b06d020970099c1107
Maaz Hassan <h.maaz809@hotmail.com>
2024-03-03 12:24:37 -0700

    build: point some sample blogs to same image
    So as to not clutter up the images folder with uneccessary duplicates.

--- db/seed_db.sql
+++ db/seed_db.sql
@@ -1,9 +1,9 @@
 \c vivid;
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Sample Blog Post 1', 'sample-blog-post-1', '<p>This is a sample blog post content in HTML format.</p>', 'sample-blog-post-1.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Sample Blog Post 1', 'sample-blog-post-1', '<p>This is a sample blog post content in HTML format.</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Sample Blog Post 2', 'sample-blog-post-2', '<p>This is a sample blog post content in HTML format, again.</p>', 'sample-blog-post-2.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Sample Blog Post 2', 'sample-blog-post-2', '<p>This is a sample blog post content in HTML format, again.</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
 VALUES ('Dragons and Dungeons', 'dragons-and-dungeons', '<p>The new and improved D&D.</p>', 'dragons-and-dungeons.jpg', '2024-02-29', '2024-02-29', '2024-02-29');
@@ -15,7 +15,7 @@ INSERT INTO Blogs (title, slug, content, image, published_at, created_at, update
 VALUES ('Burden of Memes', 'burden-of-memes', '<p>Will Bosi gave me an exclusive interview (in my dreams). Here''s what I learned...</p>', 'burden-of-memes.jpg', '2024-02-29', '2024-02-29', '2024-02-29');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('Random Thoughts', 'random-thoughts', '<p>Your stomach thinks all potatoes are mashed...</p>', 'random-thoughts.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('Random Thoughts', 'random-thoughts', '<p>Your stomach thinks all potatoes are mashed...</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
 
 INSERT INTO Blogs (title, slug, content, image, published_at, created_at, updated_at)
-VALUES ('More Random Thoughts', 'more-random-thoughts', '<p>Someone studying atoms is just a bunch of atoms trying to understand themselves...</p>', 'more-random-thoughts.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
+VALUES ('More Random Thoughts', 'more-random-thoughts', '<p>Someone studying atoms is just a bunch of atoms trying to understand themselves...</p>', 'mountains.jpeg', '2024-02-29', '2024-02-29', '2024-02-29');
